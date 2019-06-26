372abec9e1966318c7120c59262544baf40330d3
AleOchoaDev <51039153+AlejandroOchoaDev@users.noreply.github.com>
2019-06-25 21:47:45 -0500

    Update db.js
    cambio de base de dato

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-// const connectionString = 'mongodb+srv://ale8a:74ls10nnand@ale8a-3dkga.mongodb.net/test?retryWrites=true&w=majority'
 const connectionString = 'mongodb+srv://aleochoa:clave@db-pets-uilpa.mongodb.net/test?retryWrites=true&w=majority'
 
 const connect = () => new Promise((resolve, reject) => {
